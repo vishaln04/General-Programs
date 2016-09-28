e0e8ad7a76a35fb6dceeb046ca72526bf6ad426c
Vishal Nagarale <vishalnagarale@gmail.com>
2016-09-28 08:36:05 +0530

    Constrained Least Square
    This technique is implemented in c. Initialize vector M and Alpha and get the vectors from the user.

---
+++ constraint_least_square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<malloc.h>
+void main() {
+	int **M, n, i, j;
+	printf("Enter size of M matrix :\n");
+	scanf("%d",&n);
+	*M = (int**)malloc(n*sizeof(int*));
+	M = (int*)malloc(n*sizeof(int));
+	int *a = (int*)malloc(n*sizeof(int));
+	printf("Enter vector M elements : \n");
+	for (i = 0; i < n; i++)
+		for(j = 0; j < n; j++)
+			scanf("%d",&M[i][j]);
+	printf("Enter assumed vector alpha :\n");
+	for(i = 0; i < n; i++)
+		scanf("%d", a[i]);
+}
