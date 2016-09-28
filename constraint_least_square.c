128936f0e626983b3eae1d0b2d5245bc5ee544d2
Vishal Nagarale <vishalnagarale@gmail.com>
2016-09-28 17:05:45 +0530

    Dynamic Allocation correction

--- constraint_least_square.c
+++ constraint_least_square.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<malloc.h>
 void main() {
-	int **M, n, i, j;
+	int n, i, j;
 	printf("Enter size of M matrix :\n");
 	scanf("%d",&n);
-	*M = (int**)malloc(n*sizeof(int*));
-	M = (int*)malloc(n*sizeof(int));
+	int **M = (int**)malloc(n*sizeof(int*));
+	for(i=0; i<n; i++)
+		M[i] = (int*)malloc(n*sizeof(int));
 	int *a = (int*)malloc(n*sizeof(int));
 	printf("Enter vector M elements : \n");
 	for (i = 0; i < n; i++)
@@ -13,5 +14,5 @@ void main() {
 			scanf("%d",&M[i][j]);
 	printf("Enter assumed vector alpha :\n");
 	for(i = 0; i < n; i++)
-		scanf("%d", a[i]);
+		scanf("%d", &a[i]);
 }
