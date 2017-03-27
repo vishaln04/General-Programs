b6247d4c1a735fe87169d59ce82b21abf3a9c06e
Vishal Nagarale <vishaln04@users.noreply.github.com>
2017-03-27 06:23:04 +0530

    braces added to file

--- constraint_least_square.c
+++ constraint_least_square.c
@@ -5,14 +5,18 @@ void main() {
 	printf("Enter size of M matrix :\n");
 	scanf("%d",&n);
 	int **M = (int**)malloc(n*sizeof(int*));
-	for(i=0; i<n; i++)
+	for(i=0; i<n; i++) {
 		M[i] = (int*)malloc(n*sizeof(int));
+	}
 	int *a = (int*)malloc(n*sizeof(int));
 	printf("Enter vector M elements : \n");
-	for (i = 0; i < n; i++)
-		for(j = 0; j < n; j++)
+	for (i = 0; i < n; i++) {
+		for(j = 0; j < n; j++) {
 			scanf("%d",&M[i][j]);
+		}
+	}
 	printf("Enter assumed vector alpha :\n");
-	for(i = 0; i < n; i++)
+	for(i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
+	}
 }
